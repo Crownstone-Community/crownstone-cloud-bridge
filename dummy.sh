5b03771c7deb85cba5ed0ef861e284cce458a188
vliedel <bart@crownstone.rocks>
2022-12-16 14:11:34 +0100

    Print env vars.

--- dummy.sh
+++ dummy.sh
@@ -1,3 +1,9 @@
+echo "Starting dummy cloud bridge"
+echo "CLOUD_V1_URL: ${CLOUD_V1_URL}"
+echo "CLOUD_V2_URL: ${CLOUD_V2_URL}"
+echo "SSE_URL:      ${SSE_URL}"
+echo "WEBHOOK_URL:  ${WEBHOOK_URL}"
+
 while true; do
 	sleep 1
 done
